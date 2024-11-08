12497acef4fb10c6ecd326e1da4f060969d87b95
Alexis Crocilla <alexis.crocilla@epitech.digital>
2024-11-08 15:14:18 +0400

    ex_02.js DONE

---
+++ Day 02/ex_02.js
@@ -0,0 +1,13 @@
+// Déclaration des variables
+var bool = true;
+var string = "forty two";
+var integer = 42;
+var this_is_null = null;
+var float = 42.42;
+
+// Affichage de chaque variable
+displayThisText(bool);
+displayThisText(string);
+displayThisText(integer);
+displayThisText(this_is_null);
+displayThisText(float);
