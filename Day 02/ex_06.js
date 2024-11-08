830702353077487c3c1fd9b2c774a3ffeb5bdf0e
Alexis Crocilla <alexis.crocilla@epitech.digital>
2024-11-08 15:46:59 +0400

    ex_06.js DONE

---
+++ Day 02/ex_06.js
@@ -0,0 +1,8 @@
+// Vérification de la valeur de bananasCount et affichage du message
+if (bananasCount === 0) {
+    displayThisText("Oh no, there is no banana.");
+} else if (bananasCount > 0) {
+    displayThisText("Yummy!");
+} else if (bananasCount < 0) {
+    displayThisText("Do I owe you bananas?");
+}
