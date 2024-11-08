04bca5475f3f9645ad4665d38256ca0b01cc55ac
Alexis Crocilla <alexis.crocilla@epitech.digital>
2024-11-08 16:02:32 +0400

    ex_08.js DONE

---
+++ Day 02/ex_08.js
@@ -0,0 +1,10 @@
+// Vérification des conditions et affichage du message
+if (breadCount >= 2 && hamCount >= 1 && tunaCount >= 1) {
+    displayThisText("I can make a royal sandwich!");
+} else if (breadCount >= 2 && hamCount >= 1) {
+    displayThisText("I can make a ham sandwich!");
+} else if (breadCount >= 2 && tunaCount >= 1) {
+    displayThisText("I can make a tuna sandwich!");
+} else {
+    displayThisText("I'd rather be fasting tonight.");
+}
