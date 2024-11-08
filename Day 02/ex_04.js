103b4c153b542c1198c5aebba216ba84c8cddb8c
Alexis Crocilla <alexis.crocilla@epitech.digital>
2024-11-08 15:21:15 +0400

    ex_04.js DONE

---
+++ Day 02/ex_04.js
@@ -0,0 +1,17 @@
+// Déclaration de différentes variables
+var undefinedVar;
+var booleanVar = true;
+var numberVar = 42;
+var bigIntVar = 1234567890123456789012345678901234567890n;
+var symbolVar = Symbol("sym");
+var objectVar = {};
+var functionVar = function() {};
+
+// Affichage de chaque type
+displayThisText(typeof undefinedVar);
+displayThisText(typeof booleanVar);
+displayThisText(typeof numberVar);
+displayThisText(typeof bigIntVar);
+displayThisText(typeof symbolVar);
+displayThisText(typeof objectVar);
+displayThisText(typeof functionVar);
