5a3d91d655f7c636843e4bc1d6cb3575ecc06f8f
Alexis Crocilla <alexis.crocilla@epitech.digital>
2024-11-08 14:28:43 +0400

    ex_04.js DONE

---
+++ Day 01/ex_04.js
@@ -0,0 +1,5 @@
+// Affichage du message dans la page HTML
+document.body.innerHTML = "Welcome to the javascript pool!<br>Have some fun and dont forget to bring a towel.";
+
+// Affichage du message dans la console
+console.log("Website launched!");
