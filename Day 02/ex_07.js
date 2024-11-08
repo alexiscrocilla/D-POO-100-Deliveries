873f042702316c849975209e51b9556cbeb984a4
Alexis Crocilla <alexis.crocilla@epitech.digital>
2024-11-08 15:50:47 +0400

    ex_07.js

---
+++ Day 02/ex_07.js
@@ -0,0 +1,6 @@
+// Vérification de la valeur de password et affichage du message
+if (password === "forty-two") {
+    displayThisText("Success");
+} else {
+    displayThisText("Wrong password");
+}
