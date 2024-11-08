fcf35f32cc23611f6c25a086eda6e06e1c07ce58
Alexis Crocilla <alexis.crocilla@epitech.digital>
2024-11-08 15:14:32 +0400

    ex_03.js DONE

---
+++ Day 02/ex_03.js
@@ -0,0 +1,9 @@
+// Déclarer les variables
+var number1 = "1";
+var number2 = "2";
+
+// Combinaison des deux variables
+var result = number1 + number2;
+
+// Affichage du résultat
+displayThisText(result);
