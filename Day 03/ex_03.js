f34f5c7c5924337aabda69a1694fa35f6989a1b2
Alexis Crocilla <alexis.crocilla@epitech.digital>
2024-11-08 17:28:33 +0400

    ex_03.js DONE

---
+++ Day 03/ex_03.js
@@ -0,0 +1,19 @@
+function changebackground() {
+    let magicNumber = 1; // Définir magicNumber sur 1
+
+    if (magicNumber === 1) {
+        paintYellowRectangle();
+    }
+
+    magicNumber = 2; // Passer à 2
+    if (magicNumber === 2) {
+        paintGreenRectangle();
+    }
+
+    magicNumber = 3; // Passer à 3
+    if (magicNumber === 3) {
+        paintRedRectangle();
+    }
+}
+
+changebackground();
