8e1bd77a11d8c7da0a65245628789238bb7465d1
Alexis Crocilla <alexis.crocilla@epitech.digital>
2024-11-29 12:30:25 +0100

    Create ex_05.js

---
+++ Day 03/ex_05.js
@@ -0,0 +1,5 @@
+function getBasketContent(){
+    var fruits = ["strawberries","strawberries","strawberries","apples","apples","limes","limes","peach","pears","pears"]
+    console.log(fruits.length + " fruit(s) selected")
+    return fruits
+}
