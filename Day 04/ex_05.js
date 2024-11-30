3200b66f03340a4ab999838f43f95e6a9391e95e
Alexis Crocilla <alexis.crocilla@epitech.digital>
2024-11-30 11:29:09 +0100

    Create ex_05.js

---
+++ Day 04/ex_05.js
@@ -0,0 +1,10 @@
+function getHousesPrototype() {
+    const housesMap = new Map();
+    
+    housesMap.set('Apartment 75008', 90);
+    housesMap.set('Apartment 94300', 105);
+    housesMap.set('Apartment 92300', 140);
+    housesMap.set('Apartment 75015', 65);
+    
+    return housesMap;
+  }
